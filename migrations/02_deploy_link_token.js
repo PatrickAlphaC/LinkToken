21fc3bd31bdd866568744a65d5db4461f5d72373
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-03-13 12:39:12 -0500

    mumbai test worked great

--- migrations/02_deploy_link_token.js
+++ migrations/02_deploy_link_token.js
@@ -1,8 +1,8 @@
-const version = process.env.VERSION || 'v0.4'
+const version = process.env.VERSION || 'v0.6'
 
-const { LinkToken } = require(`../build/truffle/${version}/LinkToken`)
-LinkToken.setProvider(web3.currentProvider)
+const { PegSwap } = require(`../build/truffle/${version}/PegSwap`)
+PegSwap.setProvider(web3.currentProvider)
 
-module.exports = function(deployer, _, accounts) {
-  deployer.deploy(LinkToken, { from: accounts[0] })
+module.exports = function (deployer, _, accounts) {
+  deployer.deploy(PegSwap, { from: accounts[0] })
 }
